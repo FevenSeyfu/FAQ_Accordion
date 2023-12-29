0b373087884b1c0f6b5dd28774e89e271a8dd581
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-29 17:45:19 +0300

    complete toggle functionlaity

--- FAQ_Accordion/main.js
+++ FAQ_Accordion/main.js
@@ -1,1 +1,25 @@
 import './style.css'
+
+const toggleButtons= document.querySelectorAll('#plus-minus-icon')
+
+const HandleToggleIcon = (toggleIcon) => {
+    let initilaIcon = toggleIcon.src ;
+    let testIcon = initilaIcon.includes('icon-plus.svg')
+    let newImg = {
+        'true': "./assets/images/icon-minus.svg",
+        'false':"./assets/images/icon-plus.svg"
+    }[testIcon];
+    return newImg;
+}
+toggleButtons.forEach(toggleButton => {
+    toggleButton.addEventListener('click',(e)=>{
+        const faq=toggleButton.parentNode.parentElement;
+        // find and hide/show FAQ answer
+        const faqAnswer = faq.getElementsByTagName('p')[0];
+        faqAnswer.classList.toggle('hidden')
+        // find and change toggleicon 
+        const toggleIcon = toggleButton.getElementsByTagName('img')[0]
+        toggleIcon.src=HandleToggleIcon(toggleIcon)
+    })
+   
+});
