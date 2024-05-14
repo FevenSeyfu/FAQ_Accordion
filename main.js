37c1e32f59e4dde604c85a0126de1c7c38978a88
FevenSeyfu <fevenseyfu00@gmail.com>
2024-05-14 17:41:35 +0300

    add vite base url

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
+import data from "./data.json";
 
-import data from './data.json'
 const getIconPath = (imageName) => {
-  console.log(data)
   const basePath = "./assets/images";
   return `${basePath}/${imageName}`;
 };
@@ -13,17 +12,14 @@ const handleToggleIcon = (toggleIcon) => {
   return newImg;
 };
 
-fetch("./data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const faqContainer = document.getElementById("faq_container");
+const faqContainer = document.getElementById("faq_container");
 
-    data.forEach((faq, index) => {
-      const isOpen = index === 0 ? "" : "hidden";
-      const icon = index === 0 ? "icon-minus.svg" : "icon-plus.svg";
-      const hr = index !== data.length - 1 ? "<hr />" : "";
+data.forEach((faq, index) => {
+  const isOpen = index === 0 ? "" : "hidden";
+  const icon = index === 0 ? "icon-minus.svg" : "icon-plus.svg";
+  const hr = index !== data.length - 1 ? "<hr />" : "";
 
-      const faqList = `
+  const faqList = `
               <div class="faq my-2">
                   <div class="flex items-center mb-4">
                       <h2 class="font-bold text-md w-11/12 hover:text-purple-900">${
@@ -35,34 +31,35 @@ fetch("./data.json")
                           )}" alt="Faq toggle icon" />
                       </button>
                   </div>
-                  <p class="text-[16px] text-grayish-purple my-4 ${isOpen}">${ faq.answer }</p>
+                  <p class="text-[16px] text-grayish-purple my-4 ${isOpen}">${
+    faq.answer
+  }</p>
                   ${hr}
               </div>
             `;
 
-      faqContainer.innerHTML += faqList;
+  faqContainer.innerHTML += faqList;
+});
+
+const toggleButtons = document.querySelectorAll(".plus-minus-icon");
+toggleButtons.forEach((toggleButton) => {
+  toggleButton.addEventListener("click", (e) => {
+    toggleButtons.forEach((button) => {
+      const faq = button.parentNode.parentElement;
+      const faqAnswer = faq.getElementsByTagName("p")[0];
+      const toggleIcon = button.getElementsByTagName("img")[0];
+
+      if (button !== toggleButton) {
+        faqAnswer.classList.add("hidden");
+        toggleIcon.src = getIconPath("icon-plus.svg");
+      }
     });
 
-    const toggleButtons = document.querySelectorAll(".plus-minus-icon");
-    toggleButtons.forEach((toggleButton) => {
-      toggleButton.addEventListener("click", (e) => {
-        toggleButtons.forEach((button) => {
-          const faq = button.parentNode.parentElement;
-          const faqAnswer = faq.getElementsByTagName("p")[0];
-          const toggleIcon = button.getElementsByTagName("img")[0];
+    const faq = toggleButton.parentNode.parentElement;
+    const faqAnswer = faq.getElementsByTagName("p")[0];
+    const toggleIcon = toggleButton.getElementsByTagName("img")[0];
 
-          if (button !== toggleButton) {
-            faqAnswer.classList.add("hidden");
-            toggleIcon.src = getIconPath("icon-plus.svg");
-          }
-        });
-
-        const faq = toggleButton.parentNode.parentElement;
-        const faqAnswer = faq.getElementsByTagName("p")[0];
-        const toggleIcon = toggleButton.getElementsByTagName("img")[0];
-
-        faqAnswer.classList.toggle("hidden");
-        toggleIcon.src = handleToggleIcon(toggleIcon);
-      });
-    });
+    faqAnswer.classList.toggle("hidden");
+    toggleIcon.src = handleToggleIcon(toggleIcon);
   });
+});
