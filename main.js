d21b4e5f5142bff5ef0c34fffd9f8fafdf06705b
FevenSeyfu <fevenseyfu00@gmail.com>
2024-05-14 12:14:32 +0300

    optimize script for the accordion to close the old one when new one opens

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
+
 const toggleButtons= document.querySelectorAll('.plus-minus-icon')
 
 const getIconPath = (imageName) => {
-    const scriptTag = document.querySelector('script[src*="main.js"]');
-    const scriptPath = scriptTag.src;
-    const basePath = scriptPath.substring(0, scriptPath.lastIndexOf('/'));
-    return `${basePath}/assets/images/${imageName}`;
+    const basePath = './assets/images';
+    return `${basePath}/${imageName}`;
   };
   
-  const handleToggleIcon = (toggleIcon) => {
-    const initialIcon = toggleIcon.src;
-    const testIcon = initialIcon.includes('icon-plus.svg');
-    const newImg = getIconPath(testIcon ? 'icon-minus.svg' : 'icon-plus.svg');
-    return newImg;
-  };
+const handleToggleIcon = (toggleIcon) => {
+  const initialIcon = toggleIcon.src;
+  const testIcon = initialIcon.includes('icon-plus.svg');
+  const newImg = getIconPath(testIcon ? 'icon-minus.svg' : 'icon-plus.svg');
+  return newImg;
+};
 toggleButtons.forEach(toggleButton => {
-    toggleButton.addEventListener('click',(e)=>{
-        const faq=toggleButton.parentNode.parentElement;
-        // find and hide/show FAQ answer
-        const faqAnswer = faq.getElementsByTagName('p')[0];
-        faqAnswer.classList.toggle('hidden')
-        // find and change toggleicon 
-        const toggleIcon = toggleButton.getElementsByTagName('img')[0]
-        toggleIcon.src=handleToggleIcon(toggleIcon)
-    })
-   
+  toggleButton.addEventListener('click',(e)=>{
+      toggleButtons.forEach(button => {
+          const faq = button.parentNode.parentElement;
+          const faqAnswer = faq.getElementsByTagName('p')[0];
+          const toggleIcon = button.getElementsByTagName('img')[0];
+
+          if (button !== toggleButton) {
+              faqAnswer.classList.add('hidden');
+              toggleIcon.src = getIconPath('icon-plus.svg');
+          }
+      });
+
+      const faq = toggleButton.parentNode.parentElement;
+      const faqAnswer = faq.getElementsByTagName('p')[0];
+      const toggleIcon = toggleButton.getElementsByTagName('img')[0];
+
+      faqAnswer.classList.toggle('hidden');
+      toggleIcon.src = handleToggleIcon(toggleIcon);
+  });
 });
