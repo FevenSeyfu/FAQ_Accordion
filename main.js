5330a9844f529cfe76c0fe2b9b5b60eee45a7b0d
FevenSeyfu <fevenseyfu00@gmail.com>
2024-05-15 18:20:00 +0300

    refactor FAQ display and sizing to be responsive

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-import data from "./data.json";
 import plusIcon from "./assets/images/icon-plus.svg";
 import minusIcon from "./assets/images/icon-minus.svg";
 
-const getIconPath = (imageName) => {
-  const basePath = "./assets/images";
-  return `${basePath}/${imageName}`;
-};
+const toggleButtons = document.querySelectorAll(".plus-minus-icon");
 
 const handleToggleIcon = (toggleIcon) => {
   const initialIcon = toggleIcon.src;
@@ -13,49 +9,24 @@ const handleToggleIcon = (toggleIcon) => {
   const newImg = testIcon ? minusIcon : plusIcon;
   return newImg;
 };
+toggleButtons.forEach(toggleButton => {
+  toggleButton.addEventListener('click',(e)=>{
+      toggleButtons.forEach(button => {
+          const faq = button.parentNode.parentElement;
+          const faqAnswer = faq.getElementsByTagName('p')[0];
+          const toggleIcon = button.getElementsByTagName('img')[0];
 
-const faqLists = document.getElementById("faqLists");
+          if (button !== toggleButton) {
+              faqAnswer.classList.add('hidden');
+              toggleIcon.src = plusIcon;
+          }
+      });
 
-data.forEach((faq, index) => {
-  const isOpen = index === 0 ? "" : "hidden";
-  const icon = index === 0 ? minusIcon : plusIcon;
-  const hr = index !== data.length - 1 ? "<hr />" : "";
+      const faq = toggleButton.parentNode.parentElement;
+      const faqAnswer = faq.getElementsByTagName('p')[0];
+      const toggleIcon = toggleButton.getElementsByTagName('img')[0];
 
-  const faqList = `
-              <div class="faq my-2">
-                  <div class="flex items-center mb-4">
-                      <h2 class="font-bold text-md w-11/12 hover:text-purple-900">${faq.question}</h2>
-                      <button class="plus-minus-icon">
-                          <img src="${icon}" alt="Faq toggle icon" />
-                      </button>
-                  </div>
-                  <p class="text-[16px] text-grayish-purple my-4 ${isOpen}">${faq.answer}</p>
-                  ${hr}
-              </div>
-            `;
-
-  faqLists.innerHTML += faqList;
-});
-
-const toggleButtons = document.querySelectorAll(".plus-minus-icon");
-toggleButtons.forEach((toggleButton) => {
-  toggleButton.addEventListener("click", (e) => {
-    toggleButtons.forEach((button) => {
-      const faq = button.parentNode.parentElement;
-      const faqAnswer = faq.getElementsByTagName("p")[0];
-      const toggleIcon = button.getElementsByTagName("img")[0];
-
-      if (button !== toggleButton) {
-        faqAnswer.classList.add("hidden");
-        toggleIcon.src = plusIcon;
-      }
-    });
-
-    const faq = toggleButton.parentNode.parentElement;
-    const faqAnswer = faq.getElementsByTagName("p")[0];
-    const toggleIcon = toggleButton.getElementsByTagName("img")[0];
-
-    faqAnswer.classList.toggle("hidden");
-    toggleIcon.src = handleToggleIcon(toggleIcon);
+      faqAnswer.classList.toggle('hidden');
+      toggleIcon.src = handleToggleIcon(toggleIcon);
   });
 });
